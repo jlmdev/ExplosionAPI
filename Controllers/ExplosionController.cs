71872faaf67b24add89c446b24da9c04a307d436
Josh Mann <jlmann72@gmail.com>
2020-09-02 16:14:54 -0400

    Explorer Mode Complete

--- Controllers/ExplosionController.cs
+++ Controllers/ExplosionController.cs
@@ -11,5 +11,25 @@ namespace ExplosionAPI.Controllers
     [ApiController]
     public class ExplosionController : ControllerBase
     {
+        [HttpGet("{s}")]
+        public string Explode(string s)
+        {
+            // Given a string of digits (s), return a string
+            // where each digit is repeated a number of times equal to its value
+            var output = "";
+            for (var index = 0; index < s.Length; index++)
+            {
+                var sCharacter = s[index];
+                var sCharacterString = sCharacter.ToString();
+                var sInteger = Int32.Parse(sCharacterString);
+
+                for (var addNumber = 0; addNumber < sInteger; addNumber++)
+                {
+                    output += sCharacter;
+                }
+
+            }
+            return output;
+        }
     }
 }
--- Controllers/MumblingController.cs
+++ Controllers/MumblingController.cs
@@ -11,5 +11,31 @@ namespace ExplosionAPI.Controllers
     [ApiController]
     public class MumblingController : ControllerBase
     {
+        [HttpGet("{s}")]
+        public String Accum(string s)
+        {
+            var output = "";
+            for (var index = 0; index < s.Length; index++)
+            {
+                var character = s[index];
+                var characterString = character.ToString();
+                for (var counter = 0; counter <= index; counter++)
+                {
+                    if (counter == 0)
+                    {
+                        output += characterString.ToUpper();
+                    }
+                    else
+                    {
+                        output += characterString.ToLower();
+                    }
+                }
+                if (index != (s.Length - 1))
+                {
+                    output += "-";
+                }
+            }
+            return output;
+        }
     }
 }
